3458ff1518619990774953f553d59d2d5129d38c
Black-tech123 <egbeboris2@gmail.com>
2026-08-06 13:05:37 +0100

    add home

--- src/Program.vb
+++ src/Program.vb
@@ -5,6 +5,10 @@ Module Program
     Sub Main()
         Application.EnableVisualStyles()
         Application.SetCompatibleTextRenderingDefault(False)
-        Application.Run(New SplashForm())
+        ' Show the splash first as a dialog. When it closes, show the login form as the main application form.
+        Dim splash As New SplashForm()
+        splash.ShowDialog()
+
+        Application.Run(New Form1())
     End Sub
 End Module
